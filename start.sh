500c840c14094cbca78e5a10ac8de08f471d2d0a
Bosskiller665 <95178977+Bosskiller665@users.noreply.github.com>
2022-03-10 19:38:21 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 GREEN='\033[0;31m'
 while : 
 do
-echo "${GREEN} INICIANDO O P7 - BOT 2.0 
+echo "${GREEN} INICIANDO O Bosskiller - BOT 2.0 
 ESTE ARQUIVO É PROGRAMADO PARA O BOT NÃO CAIR
 APÓS ALGUMA QUEDA DE CONEXÃO"
     npm start
